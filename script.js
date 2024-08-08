36fd0896d1077da95e3d571a0e3422e09e711383
lorenafelixfe <111133805+lorenafelixfe@users.noreply.github.com>
2024-08-08 08:32:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const perguntas = [
+    {
+        enunciado: 'pergunta1',
+        alternativas:['Alternativas','alternativa2']
+
+        enunciado: 'pergunta2',
+        alternativas:['Alternativas','alternativa2']
+
+        enunciado: 'pergunta3',
+        alternativas:['Alternativas','alternativa2']
+        
+    }
+
+
+
+
+
+
+
+
+
+]
+const perguntas = [//abre a lista de objetos (itens)
+    {//abre o item
+        enunciado:"Pergunta 1",
+        alternativas:[
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+]
